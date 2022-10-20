3889a9eeb3d0a206a1b49a7228fe7e228273f147
FDC.Arnel <87960659+arnelromera@users.noreply.github.com>
2022-10-20 23:53:08 +0800

    New Update

--- Machetalk_Live/src/Open_Broadcast.java
+++ Machetalk_Live/src/Open_Broadcast.java
@@ -13,24 +13,26 @@ public class Open_Broadcast {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--use-fake-ui-for-media-stream=10");
 		
-	
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\fdcar\\Downloads\\Automatio\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		driver = new ChromeDriver(options);
 		driver.get("https://dev-front.machetalk.jp/liver/");
 		driver.manage().window().maximize();
+		
 		driver.findElement(By.className("login")).click();
-		driver.findElement(By.name("login_mail")).sendKeys("rktest1@gmail.com");
-		driver.findElement(By.name("login_password")).sendKeys("admin");
-		driver.findElement(By.className("btn_style")).click();
-//		Thread.sleep(1000);
-//		driver.findElement(By.id("doGetBonus")).click();
-//		Thread.sleep(1000);
-//		driver.findElement(By.cssSelector("button.btn_style")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.className("button")).click();
-		driver.findElement(By.id("count_text")).sendKeys("FDC TEST");
+		
+		Login test = new Login();
+		test.login(driver);
+		
+//		Register test = new Register();
+//		test.register();
+		
+		//Bonus("login", driver);
+		Thread.sleep(1000);
+		Bonus("dailybonus", driver);
+		driver.findElement(By.xpath("//a[@class='button size_m']")).click();
+		driver.findElement(By.id("count_text")).sendKeys("automated broadcast");
 		driver.findElement(By.xpath("//ul/li[2]")).click();
 		driver.findElement(By.cssSelector("button.btn_style")).click();
 		Thread.sleep(6000);
@@ -60,7 +62,66 @@ public class Open_Broadcast {
 		Thread.sleep(5000);
 		driver.findElement(By.id("btn-invite")).click();
 		
-
 	}
+	
+	private static void Bonus(String act, WebDriver driver) throws InterruptedException
+	{
+		switch (act) {
+		case "dailybonus" :
+			int d = 5;
+			
+			try {
+				boolean doGetBonusPresence = driver.findElement(By.id("doGetBonus")).isDisplayed();
+				boolean doGetBonusEnabled = driver.findElement(By.id("doGetBonus")).isDisplayed();
+				if(doGetBonusPresence == true && doGetBonusEnabled == true)	
+				{
+					driver.findElement(By.id("doGetBonus")).click();
+					Thread.sleep(1000);
+					driver.findElement(By.cssSelector("button.btn_style")).click();			
+				}
+			}
+			catch(Exception e){
+				
+				for(int i=0; i<d; i++) {
+					System.out.println("Teset");
+				}
+			}
+			break;
+			
+		case "register" :
+			break;
+			
+		case "login" :
+			driver.findElement(By.className("login")).click();
+			driver.findElement(By.name("login_mail")).sendKeys("rktest6@gmail.com");
+			driver.findElement(By.name("login_password")).sendKeys("admin");
+			driver.findElement(By.className("btn_style")).click();
+			break;
+		  default :
+				break;
+		}
+//		//check is the modal is displayed
+//		try {
+//			boolean doGetBonusPresence = driver.findElement(By.id("doGetBonus")).isDisplayed();
+//			boolean doGetBonusEnabled = driver.findElement(By.id("doGetBonus")).isDisplayed();
+//			if(doGetBonusPresence == true && doGetBonusEnabled == true)	
+//			{
+//				driver.findElement(By.id("doGetBonus")).click();
+//				Thread.sleep(1000);
+//				driver.findElement(By.cssSelector("button.btn_style")).click();
+//				return true;
+//			}
+//		} 
+//		catch(Exception e){
+//			return false;
+//		}
+		
+
+		
+		
+	}
+	
+	
+	
 
 }
